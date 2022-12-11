140655fa0060daf913b2bf7a090a2f37c83c6aa6
Đinh Xuân Trọng <104368873+TrongDx@users.noreply.github.com>
2022-12-11 20:17:21 +0700

    Add files via upload
    create code

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Huệ Sừn';
+const giftUrl = 'https://trongdx-023-my-website2.on.drv.tw/myWebsite/?fbclid=IwAR03ft0XoEKK9kXRIduCAAhgdV_bbS_Fy40iaEsdRr3eDfdp8fnTgOayt94';
+const eventName = 'Happy Birthday';
+const titleCard = 'Tặng Huệ Sừnnn';
+const contentCard = 'Chúc Huệ tuổi mới tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hpbd.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
