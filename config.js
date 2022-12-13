041812231cfbff544117f13998961f9995db5104
Đinh Xuân Trọng <104368873+TrongDx@users.noreply.github.com>
2022-12-13 20:52:39 +0700

    Update config.js
    update pic

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const contentCard = 'Chúc Huệ tuổi mới tràn ngập niềm vui và nhữn
 // Cần hỗ trợ hãy liên hệ: 
 // Mr-Nam http://facebook.com/nam.nodemy
 // Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
-const giftImage = 'hpbd.png';
+const giftImage = 'anh-sinh-nhat-che-9.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
